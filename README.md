6cfce182067de7ea0a48999f9426c9b6b2fea072
Trinmar Boado <timi.boado@gmail.com>
2019-07-29 10:05:41 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,106 @@
+[![npm version](https://badge.fury.io/js/feathers-nedb-puzzy-search.svg)](https://badge.fury.io/js/feathers-nedb-puzzy-search)
+
+# feathers-nedb-fuzzy-search
+Add google-like `$search` to NeDB `service.find` queries.
+
+<p align="center">
+  <img width="300" src="search.gif"/>
+</p>
+
+## Install
+```
+npm install feathers-nedb-puzzy-search
+```
+
+## Usage
+```js
+const search = require('feathers-nedb-puzzy-search')
+const messages = app.service('messages')
+
+// enable for message service, may use app.hooks too
+messages.hooks({
+  before: {
+    find: search({['name', 'email']})
+  }
+})
+
+let res = await service.find({ query: { name: { $search: 'ello' } } })
+let res = await service.find({ query: $search: 'ello' } })
+```
+Besure to whitelist non-standard query parameters in your model.
+That's `['$text', '$regex']` for MongoDB , and `['$where', '$regex']` for NeDB. 
+
+### Options
+`feathers-nedb-fuzzy-search` take two options in NeDB `$where` mode:
+
+```js
+search({
+  fields: ['search.this.path', 'this.path.too', 'title'],
+  deep: true
+})
+```
+
+- `fields` - Specify which fields to search.
+- `deep`- If true and `fields` is undefined, will search deep in objects.
+
+In NeDB `$regex` mode it takes as service options the following:
+
+- `excludeFields` - Specify which fields to exclude from search.
+- `fields` - Specify which fields to search. Mutually exclusive. 
+
+As query parameters it also takes `$caseSensitive`
+
+### Complete example
+```js
+const feathers = require('feathers')
+const hooks = require('feathers-hooks')
+const NeDB = require('nedb')
+const service = require('feathers-nedb')
+const search = require('feathers-nedb-fuzzy-search')
+
+const Model = new NeDB({
+  filename: './example.db',
+  autoload: true
+})
+
+const app = feathers()
+app.configure(hooks())
+app.use('/test', service({ Model }))
+app.hooks({
+  before: {
+    find: search({
+      // if omitted, then it will search all properties of documents
+      fields: ['title', 'description']
+    })
+  }
+})
+
+// you need node v7 or above for async / await syntax
+async function testDatabase () {
+  let service = app.service('test')
+  await service.create([
+    { 'title': 'asdf' },
+    { 'title': 'qwerty' },
+    { 'title': 'zxcvb' },
+    { 'title': 'hello world' },
+    { 'title': 'world around' },
+    { 'title': 'cats are awesome' },
+  ])
+
+  let res = await service.find({ query: { $search: 'world' } })
+  let res = await service.find({ query: { title: { $search: 'ello' } } })
+
+  console.log(res)
+  // [ { title: 'world around', _id: '1RDM5BJWX4DWr1Jg' },
+  //   { title: 'hello world', _id: 'dX4bpdM1IsAFkAZd' } ]
+  //   { title: 'hello world', _id: 'dX4bpdM1IsAFkAZd' } ]
+}
+
+testDatabase()
+  .catch(e => console.error(e))
+```
+
+## License
+ISC © 2019 Trinmar Boado
+MIT © 2019 Ray Foss
+MIT © 2017 Arve Seljebu
---
+++ index.js
@@ -0,0 +1,188 @@
+const escapeStrRx = require('escape-string-regexp')
+const objectPath = require('object-path')
+const deepReduce = require('deep-reduce')
+const utils = require('feathers-commons')
+const errors = require('feathers-errors')
+var Typo = require("typo-js");
+var dictionary = new Typo( "en_US" );
+
+function transformSearchFieldsInQuery (query, options, fieldName) {
+  utils.each(query, (value, key) => {
+    if (key === '$text') { // MongoDB only, passthrough
+      return
+    }
+    // Process current attribute or  recurse
+    if (value && typeof value === 'object') {
+      // Searchable field ?
+      if (!value.hasOwnProperty('$search')) {
+        return transformSearchFieldsInQuery(value, options, key)
+      }
+      // Field should either be included, or not excluded
+      if ((options.fields.length && !options.fields.includes(key)) ||
+          options.excludedFields.includes(key)) {
+          debugger
+        throw new errors.BadRequest('You are not allowed to perform $search on field ' + key)
+      }
+      transformSearchFieldsInQuery(value, options, key)
+    } else if (key === '$search') {
+      // Default to case insensitive if not given
+      // Sanitize when required
+      if (!options.fieldsNotEscaped.includes(fieldName)) {
+        value = escapeStrRx(value)
+      }
+      // Update query
+      if (fieldName) {
+        query['$regex'] = query.$caseSensitive ? new RegExp(value) : new RegExp(value, 'i')
+      } else {
+        query.$where = fuzzySearch(query.$search, options)
+      }
+      // Delete unused field
+      delete query['$search']
+      delete query['$caseSensitive']
+    }
+  })
+}
+
+function regexFieldSearch (sm) {
+  return function (hook) {
+    transformSearchFieldsInQuery(hook.params.query, sm)
+  }
+}
+
+// Passthough to NeDB driver
+function fullTextSearch (sm) {
+  return function (hook) {
+    if (hook.id || !hook.params.query || !hook.params.query.$search) {
+      return hook
+    }
+    const query = hook.params.query
+    
+    if (hook.method === 'find') {
+      query.$where = fuzzySearch(query.$search, sm)
+    }
+    delete query.$search
+
+    // Not Supported by NeDB, but supported by mongo
+    if (query.$language) {
+      delete query.$language
+    }
+    if (query.$caseSensitive) {
+      delete query.$caseSensitive
+    }
+    if (query.$diacriticSensitive) {
+      delete query.$diacriticSensitive
+    }
+
+    return hook
+  }
+}
+
+/**
+ * Add $search to `service.find` query. If `options.fields`
+ * is omitted, it will search all properties of documents.
+ *
+ * Usage:
+ * ```
+ * const fuzzySearch = require('feathers-nedb-fuzzy-search')
+ *
+ * app.service('something').hooks({
+ *   before: {
+ *     find: [ fuzzySearch({
+ *       fields: ['title', 'description']  // search these properties in the document
+ *     }) ]
+ *   }
+ * })
+ *
+ * app.service('something').find({
+ *  query: {
+ *    $search: 'text string to search for'
+ *  }
+ * })
+ * ```
+ *
+ * @param {object} options
+ */
+function init (opts = {}) {
+  const sm = {}
+  Object.assign(sm, opts)
+  // if escape is undefined -> escape = true
+  sm.escape = sm.escape !== false
+  sm.fieldsNotEscaped = Array.isArray(sm.fieldsNotEscaped) ? sm.fieldsNotEscaped : []
+  // hook for field-based search:
+  if (Array.isArray(sm.fields) && sm.fields.length) {
+    sm.excludedFields = []  // one should not both include and exclude fields
+  } else if (Array.isArray(sm.excludedFields) && sm.excludedFields.length) {
+    sm.fields = []
+  } else {
+    return fullTextSearch(sm)
+  }
+  
+  return regexFieldSearch(sm)
+}
+
+module.exports = init
+
+
+/**
+ * Returns a $where function for NeDB. The function search all
+ * properties of objects and returns true if `str` is found in
+ * one of the properties. Searching is not case sensitive.
+ *
+ * @param {string} str search for this string
+ * @return {function}
+ */
+
+function suggestionsMatched(str, value) {
+  const r1 = new RegExp('(?=.*' + escapeStrRx(str).replace(/ +?/g, ')(?=.*') + ').+', 'ig');
+  
+  if (value.match(r1) !== null) return true
+
+  let keywords = str.split(/ +?/g);
+  for (var i = 0, len = keywords.length; i < len; i++) {
+    let suggestion = dictionary.suggest(keywords[i])
+    if (suggestion.length)
+      keywords[i] = suggestion[0];
+  }
+  let str2 = keywords.join(' ');
+  const r2 = new RegExp('(?=.*' + escapeStrRx(str2).replace(/ +?/g, ')(?=.*') + ').+', 'ig');
+
+  return (value.match(r2) !== null);
+}
+
+function fuzzySearch(str, { fields, deep }) {
+  if (Array.isArray(fields)) {
+    return function () {
+      return fields.reduce((match, field) => {
+        if (match === true) {
+          return true
+        }
+        let value = objectPath.get(this, field)
+        return typeof value === 'string' && suggestionsMatched(str, value)
+      }, false)
+    }
+  }
+
+  if (deep) {
+    return function () {
+      let result = deepReduce(this, (match, value) =>
+        match || (
+          typeof value === 'string' && suggestionsMatched(str, value)
+        ), false)
+      return result
+    }
+  }
+
+  return function () {
+    for (let key in this) {
+      // do not search _id and similar fields
+      if (key[0] === '_' || !this.hasOwnProperty(key)) {
+        continue
+      }
+      if (typeof this[key] === 'string' && this[key].match(r)) {
+        return true
+      }
+    }
+    return false
+  }
+
+}
